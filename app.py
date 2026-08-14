72ec5bf2f968f7d58cab40a490d9f556b82c1a6d
Maganti Bhavana <maganti.bhavana@solugenix.com>
2026-08-14 15:47:07 +0530

    Add CI pipeline

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-from flask import Flask
+def add(a, b):
+    #return a - b #intentionally broke
+    return a + b #corrected back
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return """
-    <h1>Hello from AWS CodeDeploy!</h1>
-    <h2>Application started successfully</h2>
-    """
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+print(add(2, 3))
--- test_app.py
+++ test_app.py
@@ -1,2 +1,7 @@
-def test_app():
-    assert True
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+
+print("All tests passed")
